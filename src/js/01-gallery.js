78970959195f0009a701b61f2800f8dd2a62e6d7
Serhii151 <122047001+Serhii151@users.noreply.github.com>
2023-05-26 13:16:28 +0300

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -6,27 +6,29 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 const galleryContainer = document.querySelector('.gallery');
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
 
 
-const markup = galleryItems
-  .map(({ preview, original, description }) => {
-    return `<div class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      alt="${description}"
-    />
-  </a>
-</div>`;
-  })
-  .join('');
-
+function createGalleryItemsMarkup(items) {
+  return items.map(({ preview, original, description }) => {
+    return `
+      <a class="gallery__item" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+        />
+      </a>
+    `;
+  }).join('');
+}
 galleryContainer.insertAdjacentHTML('beforeend', markup);
 
 const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
-  captionDelay: 250,
+  captionPosition: 'bottom',
+  captionDelay: 250
 });
 
 console.log(galleryItems);
